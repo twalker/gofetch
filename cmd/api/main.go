54aa5f714ddf38963fd42e4a3a6846a1fa69272b
Tim Walker <tiwalker@starbucks.com>
2025-04-22 19:28:14 -0700

    Set default logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"log"
 	"log/slog"
 	"os"
 
+	_ "github.com/joho/godotenv/autoload"
 	"gofetch.timwalker.dev/internal/env"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	cfg := config{
 		port: env.GetInt("PORT", 4000),
 		env:  env.GetString("ENV", "local"),
@@ -24,13 +17,16 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 	})).With("pid", os.Getpid(), "name", "gofetch")
+	slog.SetDefault(logger)
 
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
+
 	mux := app.registerRoutes()
-	err = app.serve(mux)
+
+	err := app.serve(mux)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
