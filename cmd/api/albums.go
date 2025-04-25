a6ccb15e07fa4e8c4e1bd81b6e8c2e209e96d5f4
Tim Walker <tiwalker@starbucks.com>
2025-04-25 15:23:49 -0700

    Add a POST albums route

--- cmd/api/albums.go
+++ cmd/api/albums.go
@@ -11,18 +11,33 @@ type Album struct {
 	Title  string `json:"title"`
 }
 
-func (app *application) getAlbumsWithClientHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) getAlbumsFromApiClientHandler(w http.ResponseWriter, r *http.Request) {
 	var albums []Album // Define a struct User corresponding to the API response
 	resp, err := app.apiClient.Get(r.Context(), "/static/albums.json", &albums)
 	app.logger.Info("Received response",
 		"status", resp.StatusCode,
 	)
 	if err != nil {
-		// TODO: return status code from api response. status code mapping?
+		// TODO: return status code from api response and log error.
+		// status code mapping?
 		// app.internalServerError(w, r, err)
 		// resp.Write(w)
 		writeJSONError(w, resp.StatusCode, err.Error())
+		app.logger.Error("apiclient returned an error:", "fetchUrl", resp.Request.URL.Path, "fetchStatus", resp.StatusCode)
 		return
 	}
 	writeJSONData(w, resp.StatusCode, albums)
+}
+
+func (app *application) createAlbumHandler(w http.ResponseWriter, r *http.Request) {
+	var newAlbum Album
+	err := readJSON(w, r, &newAlbum)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	err = writeJSONData(w, http.StatusCreated, newAlbum)
+	if err != nil {
+		app.internalServerError(w, r, err)
+	}
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,8 @@ func (app *application) registerRoutes() http.Handler {
 	mux.HandleFunc("GET /health", app.healthCheckHandler)
 	mux.HandleFunc("HEAD /health", app.healthCheckHandler)
 	mux.HandleFunc("GET /errors/{code}", app.checkErrorResponseHandler)
-	mux.HandleFunc("GET /apiclient/albums", app.getAlbumsWithClientHandler)
+	mux.HandleFunc("GET /apiclient/albums", app.getAlbumsFromApiClientHandler)
+	mux.HandleFunc("POST /albums", app.createAlbumHandler)
 	// Unmatched route patters receive a 404
 	mux.HandleFunc("/", app.notFoundResponse)
 
