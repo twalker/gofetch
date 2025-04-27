8a9ad9524c9f963f00a95935caeafd5c10c22576
Tim Walker <tiwalker@starbucks.com>
2025-04-26 21:34:16 -0700

    Reflect back apiclient error status codes

--- cmd/api/albums.go
+++ cmd/api/albums.go
@@ -12,18 +12,14 @@ type Album struct {
 }
 
 func (app *application) getAlbumsFromApiClientHandler(w http.ResponseWriter, r *http.Request) {
-	var albums []Album // Define a struct User corresponding to the API response
+	var albums []Album
 	resp, err := app.apiClient.Get(r.Context(), "/static/albums.json", &albums)
-	app.logger.Info("Received response",
-		"status", resp.StatusCode,
-	)
 	if err != nil {
-		// TODO: return status code from api response and log error.
-		// status code mapping?
-		// app.internalServerError(w, r, err)
-		// resp.Write(w)
-		writeJSONError(w, resp.StatusCode, err.Error())
-		app.logger.Error("apiclient returned an error:", "fetchUrl", resp.Request.URL.Path, "fetchStatus", resp.StatusCode)
+		if resp != nil {
+			app.apiClientErrorResponse(w, r, resp.StatusCode, err)
+			return
+		}
+		app.internalServerError(w, r, err)
 		return
 	}
 	writeJSONData(w, resp.StatusCode, albums)
--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"net/http"
+	"runtime/debug"
 )
 
 // internalServerError returns a 500 error response and logs the provided error.
 func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
-	app.logger.Error("internal error", "method", r.Method, "path", r.URL.Path, "error", err.Error(), string(correlationIDContextKey), app.getCorrelationID(r.Context()))
+	app.logger.Error("internal error", "method", r.Method, "path", r.URL.Path, "error", err.Error(), string(correlationIDContextKey), app.getCorrelationID(r.Context()), "stack", string(debug.Stack()))
 
 	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
 }
@@ -37,4 +38,11 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request)
 	app.logger.Warn("not found error", "method", r.Method, "path", r.URL.Path, string(correlationIDContextKey), app.getCorrelationID(r.Context()))
 
 	writeJSONError(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
+}
+
+// apiClientErrorResponse returns the status code from the api client logs the error.
+func (app *application) apiClientErrorResponse(w http.ResponseWriter, r *http.Request, status int, err error) {
+	app.logger.Error("apiclient error", "method", r.Method, "path", r.URL.Path, "error", err.Error(), string(correlationIDContextKey), app.getCorrelationID(r.Context()), "stack", string(debug.Stack()))
+
+	writeJSONError(w, status, "the server encountered a problem")
 }
